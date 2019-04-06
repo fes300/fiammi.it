d2f73776cb45a6e8885f2ec30da50727ac20ea15
Federico Sordillo <federicosordillo@gmail.com>
2019-04-06 16:30:43 +0200

    refine build method

--- src/Components/App.purs
+++ src/Components/App.purs
@@ -2,17 +2,17 @@ module Components.App where
 
 import Prelude
 
-import Components.Toggle (toggle)
 import React.Basic (Component, JSX, createComponent, makeStateless)
-import React.Basic.DOM as R
+import React.Basic.DOM as DOM
+import React.Basic.DOM.Generated (style)
 
 component :: Component Unit
 component = createComponent "App"
 
 app :: JSX
 app = unit # makeStateless component \_ ->
-  R.div_
-    [ R.h1_ [ R.text "Hello world" ]
-    , toggle { initialValue: true }
-    , toggle { initialValue: false }
-    ]
+  DOM.div
+    {
+      style: DOM.css { padding: "5px" }
+      , children: [ DOM.text "prova" ]
+    }
